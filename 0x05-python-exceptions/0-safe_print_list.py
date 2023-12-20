93c35cb0839fc6d92a0a7947bbd40c5a01d3ea1d
Ebenie <abrahamavenue163@.com>
2023-12-20 05:51:10 -0800

    second updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,13 @@
 
 def safe_print_list(my_list=[], x=0):
     printed_elements = 0
-    
-    try:
-        while printed_elements < x:
-            print("{}".format(my_list[printed_elements]), end="")
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
             printed_elements += 1
-    except IndexError:
-        pass
+        except IndexError:
+            break
     
     print()
     return printed_elements
