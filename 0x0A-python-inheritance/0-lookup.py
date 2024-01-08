bb4f4fe33ec80d67580e59e596767a0298098276
Ebenie <abrahamavenue163@.com>
2024-01-08 12:11:26 -0800

    Still error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-def lookup(obj):
-    """Returns a list of available attributes and methods of an object."""
 
-    return list(dir(obj))
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    :param obj: The object to look up.
+    :return: A list of attributes and methods.
+    """
+    return dir(obj)
+
