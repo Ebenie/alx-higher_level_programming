bfa2ab333709a29cb2967b667491310abf41f1e1
Ebenie <abrahamavenue163@.com>
2024-01-10 12:26:20 -0800

    The advanced parts

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""Defines the Square class inherites Rectangle class"""
+"""Defines the Square class inheriting from Rectangle class"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a square class and inherits Rectangle."""
+    """Represents a square class that inherits from Rectangle"""
 
     def __init__(self, size):
-        """Initialize a new square objecect
-
-        Args:
-            size (int): The size of the new square.
-        """
+        """Initializes a square with a given size"""
         super().__init__(size, size)
+
+    def __str__(self):
+        """Returns a string representation of the square"""
+        return "[Square] {}/{}".format(self.width, self.height)
+
+    def area(self):
+        """This function calculates & returns the area of the square"""
+        return self.width * self.height
