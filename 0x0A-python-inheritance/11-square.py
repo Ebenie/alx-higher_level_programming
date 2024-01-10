4c66b627604d5cd9edf8e765deeb6cc5bf6885df
Ebenie <abrahamavenue163@.com>
2024-01-10 12:16:50 -0800

    The advanced parts

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,9 +7,9 @@ class Square(Rectangle):
     """Defines a square class and inherits Rectangle."""
 
     def __init__(self, size):
-        """Initializes a square with a given size."""
-        super().__init__(size, size)
+        """Initialize a new square objecect
 
-    def __str__(self):
-        """Returns a string of the square."""
-        return "[Square] {}/{}".format(self.width, self.height)
+        Args:
+            size (int): The size of the new square.
+        """
+        super().__init__(size, size)
