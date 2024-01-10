0161bc37ca845181996625c7d58131f1fa48a776
Ebenie <abrahamavenue163@.com>
2024-01-10 10:56:02 -0800

    Debbugging with burning

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""Defines function that checks object is an instance of class"""
+
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if the object is an instance of, or if the object is an instance of a class
-    that inherited from, the specified class; otherwise False.
+    checks the object is instance of the specified class or inherited
+
+    Args:
+    - obj: Any Python object
+    - a_class: A Python class
+
+    Returns:
+    - bool: True if obj is an instance of a_class otherwise False
     """
     return isinstance(obj, a_class)
-
