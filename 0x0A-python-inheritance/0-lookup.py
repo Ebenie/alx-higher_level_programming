b05862b2653437ae66a3233957874a06b1db73b0
Ebenie <abrahamavenue163@.com>
2024-01-10 09:51:26 -0800

    Debugging the errors

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,30 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
-    """
-    Returns a list of attributes and methods of an object.
-
-    Args:
-    - obj: Any Python object
-
-    Returns:
-    - list: List of attribute and method names
-    """
-    return [n for n in dir(obj) if not callable(getattr(obj, n)) or n.startswith("__")]
-
-
-
-class Example:
-    """
-    Defines the Example class to be inherited.
-    """
-    def __init__(self):
-        self.att = 50
-
-    def method1(self):
-        return "Method 1!"
-
-obj1 = Example()
-
-result = lookup(obj1)
-print(result)
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
