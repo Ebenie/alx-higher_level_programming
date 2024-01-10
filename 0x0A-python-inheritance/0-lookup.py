79fcf52c95c6ff3a38144406df33f8e243360d6f
Ebenie <abrahamavenue163@.com>
2024-01-10 10:05:50 -0800

    Debugging the errors

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creates a function for inspecting object attributes"""
 
+
 def lookup(obj):
     """Returns list of attributes & methods of an object"""
     return (dir(obj))
