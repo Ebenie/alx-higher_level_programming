7daab7e0064f4d9f2d8df06c056aed3d66bba32b
Ebenie <abrahamavenue163@.com>
2024-02-13 11:07:59 -0800

    0x0D-SQL_introduction solutions

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- The database name will be passed as an argument
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = `first_table`;
+WHERE TABLE_NAME = 'first_table';
