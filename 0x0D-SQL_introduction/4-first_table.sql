9934396d1f689b1b2daacbcb2b1b0617978e4ac8
Ebenie <abrahamavenue163@.com>
2024-02-13 10:58:51 -0800

    0x0D-SQL_introduction solutions

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- This command uses argument as database name 
 -- This command creates a table 'first_table'
 -- If the table exist won't fail
-CREATE TABLE IF NOT EXISTS 'first_table' (
-    'id' INT,
-    'name' VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `first_table` (
+    `id` INT,
+    `name` VARCHAR(256)
 );
