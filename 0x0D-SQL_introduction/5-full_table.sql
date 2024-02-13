d0f115b5153e39ac9317ddcd4516daf771571d9d
Ebenie <abrahamavenue163@.com>
2024-02-13 11:05:47 -0800

    0x0D-SQL_introduction solutions

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,8 +1,5 @@
 -- This display the full description of the table first_table
--- The database name will be passed as an argument so ?
-USE information_schema;
-
+-- The database name will be passed as an argument
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM COLUMNS
-WHERE TABLE_SCHEMA = ?
-AND TABLE_NAME = 'first_table';
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = `first_table`;
