5feb208711bad54c2e6a421aee5f67b0b64fee74
Ebenie <abrahamavenue163@.com>
2024-02-14 12:44:53 -0800

    The mandatory part ended

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- This command select all shows and their linked genres from the database
+
 SELECT tv_shows.title, tv_genres.name AS genre
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- This command select all cities with their corresponding state names
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
 
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
