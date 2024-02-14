bf062fe5bc28b7da4ec8e03e227422b251419141
Ebenie <abrahamavenue163@.com>
2024-02-14 10:40:45 -0800

    second push

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- This command creates user 'user_0d_1' if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- This command grants all privileges to 'user_0d_1'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
