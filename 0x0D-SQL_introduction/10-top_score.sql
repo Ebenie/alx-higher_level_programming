7a66f45428e3514fa3a09968fb1e1a81b689ee0a
Ebenie <abrahamavenue163@.com>
2024-02-15 00:44:36 -0800

    Debugging the code

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,5 @@
 -- SQL query selects the score and name columns from
 -- the second_table table and orders the results in 
---descending order based on the score column
+-- descending order based on the score column
 SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
+
