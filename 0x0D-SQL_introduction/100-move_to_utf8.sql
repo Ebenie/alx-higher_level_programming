1d6cec6d295654b4ad4ebaed07aad44dbfc640dc
Ebenie <abrahamavenue163@.com>
2024-02-15 05:20:28 -0800

    advanced part

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,9 @@
--- Convert the database to UTF8
+-- Convert database to UTF8MB4
 ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table to UTF8
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert table to UTF8MB4
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the field "name" to UTF8
-ALTER TABLE `first_table` MODIFY COLUMN `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert field within the table to UTF8MB4
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
