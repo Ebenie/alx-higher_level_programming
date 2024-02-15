5af92362d58f4d52763fe349cda654aa02a7ad7a
Ebenie <abrahamavenue163@.com>
2024-02-15 06:43:03 -0800

    This is too much difficult

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -2,9 +2,6 @@
 -- temperatures table and orders the results by temperature in 
 -- descending order
 
-USE `hbtn_0c_0`;
-source temperatures.sql;
-
 SELECT `city`, AVG(`value`) AS `average_temp` FROM `temperatures`
 GROUP BY `city`
 ORDER BY `average_temp` DESC;
