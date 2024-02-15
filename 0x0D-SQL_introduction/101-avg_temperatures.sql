fccb578102cfde738e10298a95f7da7484340c52
Ebenie <abrahamavenue163@.com>
2024-02-15 06:25:56 -0800

    iam tired of you 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,9 @@
 -- This command calculates the average temperature by city from the
 -- temperatures table and orders the results by temperature in 
 -- descending order
+
 USE `hbtn_0c_0`;
+source temperatures.sql;
 
 SELECT `city`, AVG(`value`) AS `average_temp` FROM `temperatures`
 GROUP BY `city`
