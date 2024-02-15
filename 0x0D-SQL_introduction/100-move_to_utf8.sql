c3cb27712cc8e45bc2d672d5196e6d171ae09c24
Ebenie <abrahamavenue163@.com>
2024-02-15 01:42:19 -0800

    The advanced parts

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,12 +2,7 @@
 --  hbtn_0c_0, the table first_table, and the field name to UTF-8 
 -- (utf8mb4) with the collation utf8mb4_unicode_ci.
 
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 
-COLLATE utf8mb4_unicode_ci;
-
--- This command converts the character encoding of the
--- table first_table to UTF-8 (utf8mb4) with the collation 
--- utf8mb4_unicode_ci
+USE`hbtn_0c_0` 
 ALTER TABLE `first_table` CONVERT TO CHARACTER SET
  utf8mb4 COLLATE utf8mb4_unicode_ci;
 
