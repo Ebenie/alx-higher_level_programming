c9e139410b97caa4a8de7977394dfbfbbd1bd455
Ebenie <abrahamavenue163@.com>
2024-02-15 05:52:42 -0800

    iam tired of you 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,10 @@
--- Converts the entire database hbtn_0c_0 to UTF8.
+-- This command converts `hbtn_0c_0` database to UTF8 
+-- (utf8mb4, collate utf8mb4_unicode_ci) in MySQL server
+
+--  This command uses the database `hbtn_0c_0`
 USE `hbtn_0c_0`
-ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- This command updates the database and the tabel 'first_table' 
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
+
