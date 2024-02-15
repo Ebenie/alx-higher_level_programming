e79c78ec43a3376721dfab07ef67584595038286
Ebenie <abrahamavenue163@.com>
2024-02-15 11:37:01 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -5,7 +5,7 @@
 -- the SUM() function, groups the results by the genre name using GROUP BY,
 -- and finally sorts the results in descending order by the rating sum.
 
-SELECT tv_genres.name, SUM(rating) AS rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
