741a17e56185efe6830e6bafbfae8e198220512b
Ebenie <abrahamavenue163@.com>
2024-02-15 11:21:45 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -5,9 +5,9 @@
 --  groups the results by the show's title using GROUP BY, and
 -- finally sorts the results in descending order by the rating sum.
 
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating_sum'
+SELECT title, SUM(tv_show_ratings.rate) AS 'rating_sum'
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
 ORDER BY rating_sum DESC;
 
