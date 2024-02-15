f9f6f85b8b254684c565fb04fc59e11a165d0798
Ebenie <abrahamavenue163@.com>
2024-02-15 06:59:59 -0800

    The last push of 0x0D-SQL_introduction

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,7 @@
 -- This command lists the maximum temperature of each state from 
 -- the temperatures table, ordered by state name.
-SELECT `state`, MAX(`temp`) AS `max_temp` FROM `temperatures`
+
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures`
 GROUP BY `state`
 ORDER BY `state`;
+
