3f1adb913aee86f2a1d82d90146ceeff01fa3e32
Ebenie <abrahamavenue163@.com>
2024-02-15 06:56:36 -0800

    This is too much difficult

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,7 +3,7 @@
 -- ordered by temperature in descending order
 
 SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
-WHERE month IN ('July', 'August')
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
