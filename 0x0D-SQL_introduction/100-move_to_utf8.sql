cbfdc2a017a1255aad059f72eadb565787a83def
Ebenie <abrahamavenue163@.com>
2024-02-15 01:39:41 -0800

    The advanced parts

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,3 +5,9 @@
 ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
 
+-- This command converts the character encoding of the
+-- table first_table to UTF-8 (utf8mb4) with the collation 
+-- utf8mb4_unicode_ci
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET
+ utf8mb4 COLLATE utf8mb4_unicode_ci;
+
