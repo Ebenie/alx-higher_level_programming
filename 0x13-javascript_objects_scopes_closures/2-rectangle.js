5396d8fab2a5e592380a80e3726088b4ff633eda
Ebenie <abrahamavenue163@.com>
2024-03-12 14:01:17 -0700

    updated code

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-        if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+        if (w <= 0 || h <= 0) {
             return {};
         }
         this.width = w;
         this.height = h;
     }
 }
-
 module.exports = Rectangle;
 
