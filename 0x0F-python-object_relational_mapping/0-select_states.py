255c58c5844f4a373fb5846165c360e6c7b13fcd
Ebenie <abrahamavenue163@.com>
2024-03-16 12:37:56 -0700

    Pthon ORM project solutions

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,14 @@
 import sys
 import MySQLdb
 
-def list_states(username, password, database):
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
@@ -13,15 +20,4 @@ def list_states(username, password, database):
 
     cur.close()
     conn.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python 0-select_states.py <username> <password> <database>")
-        sys.exit(1)
-    
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
 
