3a73a284b9e205542d89f95a995a2025eb1d468d
Ebenie <abrahamavenue163@.com>
2024-03-16 02:50:53 -0700

    The python ORM project solutions

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-import MySQLdb
 import sys
+import MySQLdb
+
+def list_states(username, password, database):
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python 0-select_states.py <username> <password> <database>")
+        sys.exit(1)
+    
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db.close()
+    list_states(username, password, database)
 
