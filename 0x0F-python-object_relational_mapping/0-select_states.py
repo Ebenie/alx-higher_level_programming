b212a51823fa6aa49af68a2f36addfc6f300e385
Ebenie <abrahamavenue163@.com>
2024-03-22 12:20:47 -0700

    updated version

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the database hbtn_0e_0_usa
+This script that lists all states from 
+the database hbtn_0e_0_usa
 """
 
 import sys
@@ -8,14 +9,14 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cur.fetchall():
+                         db=sys.argv[3],
+                         port=3306)
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in curs.fetchall():
         print(row)
-    cur.close()
+    curs.close()
     db.close()
 
