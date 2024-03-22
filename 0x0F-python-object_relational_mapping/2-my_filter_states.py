8c0dfb0c3d6c5af5d928ec50e6f89ac5ea151d20
Ebenie <abrahamavenue163@.com>
2024-03-22 13:42:00 -0700

    updated version

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
-"""
-
-import sys
+"""Script that lists all states from the database hbtn_0e_0_usa."""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
+    					 user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
-    for row in cur.fetchall():
+                         db=sys.argv[3], port=3306)
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = curs.fetchall()
+    for row in rows:
         print(row)
-    cur.close()
+    curs.close()
     db.close()
-
