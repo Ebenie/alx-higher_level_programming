ca4b132a74cfc479db5c8c11b2b4ba87d55ed815
Ebenie <abrahamavenue163@.com>
2024-03-28 11:03:22 -0700

    0x10-python-network_0 project solutions

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # This script fetches a URL's response body size using curl's silent mode
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+size=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
+
+echo "$size"
 
