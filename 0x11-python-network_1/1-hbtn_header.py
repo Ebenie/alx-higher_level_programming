d5cf037dbed65bcafb72de4140f361a992114924
Ebenie <abrahamavenue163@.com>
2024-03-29 10:55:38 -0700

    updated version

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-
+    req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         request_id = response.headers.get('X-Request-Id')
         print(request_id)
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 This script sends a request to a URL and displays the value of the variable X-Request-Id in the response header.
@@ -9,9 +8,7 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    
     response = requests.get(url)
     request_id = response.headers.get('X-Request-Id')
-    
     print(request_id)
 
