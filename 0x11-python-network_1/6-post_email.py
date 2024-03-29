cb18e47db7b2636a42d727dd6e904df3596eac75
Ebenie <abrahamavenue163@.com>
2024-03-29 11:40:20 -0700

    updated version

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,16 +3,13 @@
 This script sends a POST request to a URL with an email parameter and displays the body of the response.
 """
 
-import urllib.request
-import urllib.parse
+import requests
 import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print('Your email is:', response.text)
 
