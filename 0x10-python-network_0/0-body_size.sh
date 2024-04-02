bd643cb4858f305bf87b6d65915c81db551ce1e8
Ebenie <abrahamavenue163@.com>
2024-04-02 16:18:32 -0700

    updated version

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-# This script fetches a URL's response body size using curl's silent mode
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+
+# Check if a URL is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the provided URL and store the response in a temporary file
+response_file=$(mktemp)
+curl -s "$1" > "$response_file"
+
+# Calculate the size of the response body in bytes
+response_size=$(wc -c < "$response_file")
+
+# Display the size
+echo "$response_size"
+
+# Clean up the temporary file
+rm "$response_file"
 
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
-"""
-This script sends a POST request to a URL with an email parameter and displays the body of the response.
-"""
-
+"""Python script that sends a POST request to a specified URL with an email address"""
 import requests
 import sys
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    email = sys.argv[2]
+def post_email(url, email):
+    """
+    Sends a POST request to the specified URL with the email as a parameter,
+    and displays the body of the response.
+
+    Args:
+        url (str): The URL to send the POST request to.
+        email (str): The email address to be sent.
+
+    Returns:
+        None
+    """
     payload = {'email': email}
     response = requests.post(url, data=payload)
-    print('Your email is:', response.text)
+    
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Failed to send POST request.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python 6-post_email.py <URL> <email>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    email = sys.argv[2]
+    
+    post_email(url, email)
 
