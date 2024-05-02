c38e03d82d09c5faa1d27b430737db44ac573b2c
Ebenie <abrahamavenue163@gmail.com>
2024-05-02 13:15:10 -0700

    0x15-javascript-web_jquery#

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+        data.results.forEach(function (movie) {
+            $('<li>').text(movie.title).appendTo('ul#list_movies');
+        });
+    });
+});
+
