0fbf5a3795e68761e8b0354bfc6bfced6b10184d
Ebenie <124689465+Ebenie@users.noreply.github.com>
2024-05-31 13:40:52 +0300

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/2-delete.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script sends a DELETE request via curl, displaying the response body
-curl -s -X DELETE "$1"
-
--- 0x10-python-network_0/4-header.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script sends a GET request to a URL and displays the response body, including X-School-User-Id header with value 98
-curl -s -H "X-School-User-Id: 98" "$1"
-
