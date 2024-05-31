f36b10176d157b6486eed749cb550748873c69bc
Ebenie <124689465+Ebenie@users.noreply.github.com>
2024-05-31 13:27:40 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-const secondBiggest = args.length < 2 ? 0 : args.sort((a, b) => b - a)[1];
-console.log(secondBiggest);
-
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv[2];
-const num = parseInt(arg);
-console.log(isNaN(num) ? 'Not a number' : 'My number: ' + num);
-
