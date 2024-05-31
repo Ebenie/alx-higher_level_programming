9da03e9b35782f23379b105e9460f6142f02c711
Ebenie <124689465+Ebenie@users.noreply.github.com>
2024-05-31 13:28:47 +0300

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/5-script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('#add_item').click(function () {
-        $('<li>Item</li>').appendTo('ul.my_list');
-    });
-});
-
--- 0x15-javascript-web_jquery/8-script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
-        data.results.forEach(function (movie) {
-            $('<li>').text(movie.title).appendTo('ul#list_movies');
-        });
-    });
-});
-
